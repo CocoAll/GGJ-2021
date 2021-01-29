aa93574a4a796b2bdf6bb03a0c6e733a2d2fbde4
CocoAll <corentin.all@gmail.com>
2021-01-29 23:27:10 +0100

    [GGJ] start creating base archi

---
+++ Brain In Out/Assets/Script/ScriptableObjects/EnveloppeObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Enveloppe", menuName = "ScriptableObject/Enveloppe", order = 0)]
+public class EnveloppeObject : ScriptableObject
+{
+    public string titre;
+    public string contenu;
+
+}
