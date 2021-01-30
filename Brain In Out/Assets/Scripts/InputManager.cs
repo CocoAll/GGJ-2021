bee670cca9aba3f60d7091c0aa85f519ae0df213
CocoAll <corentin.all@gmail.com>
2021-01-30 08:28:26 +0100

    Start input gestion & ajout boolean par défaut dans le script timer

--- Brain In Out/Assets/Scripts/InputManager.cs
+++ Brain In Out/Assets/Scripts/InputManager.cs
@@ -12,7 +12,16 @@ public class InputManager : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
+            //On récupère la position de la souris à l'écran
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
 
+            // On raycast à la position de la souris pour un collicer 2d
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            if (hit.collider != null)
+            {
+                Debug.Log(hit.collider.gameObject.name);
+            }
         }      
     }
 }
