b636d48286fc3bb0b525a232f734ee5ed22b21de
CocoAll <corentin.all@gmail.com>
2021-01-31 13:17:40 +0100

    Update between round

--- Brain In Out/Assets/Scripts/BetweenRoundManager.cs
+++ Brain In Out/Assets/Scripts/BetweenRoundManager.cs
@@ -13,7 +13,7 @@ public class BetweenRoundManager : MonoBehaviour
     [SerializeField]
     private List<Sprite> imagesYeux;
     [SerializeField]
-    private SpriteRenderer[] rendererYeux = new SpriteRenderer[2];
+    private SpriteRenderer[] rendererYeux = new SpriteRenderer[1];
 
     [SerializeField]
     private SpriteRenderer background;
@@ -24,6 +24,14 @@ public class BetweenRoundManager : MonoBehaviour
     private IEnumerator AnimationDayPassing()
     {
         background.sprite = dayImage;
+        //Shuffle de la liste
+        for (int i = imagesYeux.Count - 1; i > 0; i--)
+        {
+            int r = Random.Range(0, i + 1);
+            Sprite tmp = imagesYeux[i];
+            imagesYeux[i] = imagesYeux[r];
+            imagesYeux[r] = tmp;
+        }
         for (int i = 0; i < imagesYeux.Count; i++)
         {
             foreach(SpriteRenderer oeil in rendererYeux)
@@ -32,11 +40,13 @@ public class BetweenRoundManager : MonoBehaviour
             }
             yield return new WaitForSeconds(0.2f);
         }
+        yield return new WaitForSeconds(0.2f);
         background.sprite = nightImage;
         foreach (SpriteRenderer oeil in rendererYeux)
         {
             oeil.sprite = null;
         }
+        yield return new WaitForSeconds(0.2f);
         startRound.Raise();
     }
 
