ba40bd24b46aad2082cf50fe821ad344ef4ee24c
LouisNMB <baron.louis.pro@gmail.com>
2021-01-31 15:31:18 +0100

    + Added end round warning if repress feelings are coming next round

---
+++ Brain In Out/Assets/Scripts/RepressedAlert.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RepressedAlert : MonoBehaviour
+{
+    [SerializeField]
+    private Text warningText;
+    [SerializeField]
+    private Color warningColor;
+    [SerializeField]
+    private Color transparent;
+    [SerializeField]
+    private TypeEnumValue overwhelmedType;
+
+
+    private void Update()
+    {
+        if (overwhelmedType.value != TypeEnum.NEUTRE)
+        {
+            float timeDiff = Time.realtimeSinceStartup - Mathf.Floor(Time.realtimeSinceStartup);
+            if (timeDiff > 0 && timeDiff < 0.2f)
+            {
+                warningText.color = transparent;
+            }
+            else
+                warningText.color = warningColor;
+        }
+    }
+}
