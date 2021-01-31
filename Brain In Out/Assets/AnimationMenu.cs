96ef82684c801c7881be964c2fbe8e8d0477523c
CocoAll <corentin.all@gmail.com>
2021-01-31 17:27:13 +0100

    Updated menu

---
+++ Brain In Out/Assets/AnimationMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimationMenu : MonoBehaviour
+{
+    public List<Sprite> imagesEnimations;
+    public Image image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Animate());
+    }
+
+    private IEnumerator Animate()
+    {
+        while (true)
+        {
+            foreach(Sprite img in imagesEnimations)
+            {
+                yield return new WaitForSeconds(0.4f);
+                image.sprite = img;
+            }
+        }
+    }
+    
+}
---
+++ Brain In Out/Assets/TutoPanel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutoPanel : MonoBehaviour
+{
+    public List<Sprite> tutos = new List<Sprite>();
+    public SpriteRenderer imageTarget;
+    int index;
+
+    private void Start()
+    {
+        index = 0;
+        if (tutos.Count != 0)
+        {
+            imageTarget.sprite = tutos[index];
+        }        
+    }
+
+    public void Next()
+    {
+        if(index + 1 < tutos.Count)
+        {
+            index++;
+            imageTarget.sprite = tutos[index];
+        }
+        else
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    public void Previous()
+    {
+        if (index - 1 >= 0)
+        {
+            index--;
+            imageTarget.sprite = tutos[index];
+        }
+    }
+}
