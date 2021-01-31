97d6da753336ac7ad01d5b1bca1e15ab1c986937
LouisNMB <baron.louis.pro@gmail.com>
2021-01-31 09:00:33 +0100

    + Neutral letters + Gauge value changed displayed with red/white colors

--- Brain In Out/Assets/Scripts/JaugeManager.cs
+++ Brain In Out/Assets/Scripts/JaugeManager.cs
@@ -19,10 +19,25 @@ public class JaugeManager : MonoBehaviour
     private SpriteRenderer goJaugeTravail;
     [SerializeField]
     private SpriteRenderer goJaugeSocial;
+    [SerializeField]
+    private SpriteRenderer goJaugeAmourTemp;
+    [SerializeField]
+    private SpriteRenderer goJaugeTravailTemp;
+    [SerializeField]
+    private SpriteRenderer goJaugeSocialTemp;
+    [SerializeField]
+    private Color increaseColor;
+    [SerializeField]
+    private Color decreaseColor;
+
+
 
     private float goAmourScaleY;
     private float goTravailScaleY;
     private float goSocialScaleY;
+    private float lastAmourScaleY;
+    private float lastTravailScaleY;
+    private float lastSocialScaleY;
 
     [SerializeField]
     private TypeEnumValue onOverwhelmed;
@@ -35,6 +50,9 @@ public class JaugeManager : MonoBehaviour
         goAmourScaleY = goJaugeAmour.transform.localScale.y;
         goTravailScaleY = goJaugeTravail.transform.localScale.y;
         goSocialScaleY = goJaugeSocial.transform.localScale.y;
+        lastAmourScaleY = goAmourScaleY / 2;
+        lastTravailScaleY = goTravailScaleY / 2;
+        lastSocialScaleY = goSocialScaleY/2;
         UpdateJauges();
     }
 
@@ -102,16 +120,79 @@ public class JaugeManager : MonoBehaviour
 
     public void UpdateJauges()
     {
-        var newScale = this.goJaugeAmour.transform.localScale;
-        newScale.y = goAmourScaleY * (jaugeAmour.Value / 100.0f);
-        this.goJaugeAmour.transform.localScale = newScale;
+        // ##### AMOUR ######
 
+        // Both gauges are set to the correct scale
+        var newScale = this.goJaugeAmour.transform.localScale;
+        newScale.y = lastAmourScaleY;
+        this.goJaugeAmour.transform.localScale = newScale;
+        this.goJaugeAmourTemp.transform.localScale = newScale;
+        // New scale is calculated
+        newScale = this.goJaugeAmour.transform.localScale;
+        newScale.y = goAmourScaleY * (jaugeAmour.Value / 100.0f);
+        // If last value > new value, value has decreased and we display red modification
+        if ((lastAmourScaleY * 100 / goAmourScaleY) > jaugeAmour.Value)
+        {
+            this.goJaugeAmourTemp.color = decreaseColor;
+            this.goJaugeAmour.transform.localScale = newScale;
+            lastAmourScaleY = newScale.y;
+        }
+        // Else if value < new value, value has increased and we display green modification
+        else if ((lastAmourScaleY * 100 / goAmourScaleY) < jaugeAmour.Value)
+        {
+            this.goJaugeAmourTemp.color = increaseColor;
+            this.goJaugeAmourTemp.transform.localScale = newScale;
+            lastAmourScaleY = newScale.y;
+        }
+
+        // ##### TRAVAIL ######
+
+        // Both gauges are set to the correct scale
+        newScale = this.goJaugeTravail.transform.localScale;
+        newScale.y = lastTravailScaleY;
+        this.goJaugeTravail.transform.localScale = newScale;
+        this.goJaugeTravailTemp.transform.localScale = newScale;
+        // New scale is calculated
         newScale = this.goJaugeTravail.transform.localScale;
         newScale.y = goTravailScaleY * (jaugeTravail.Value / 100.0f);
-        this.goJaugeTravail.transform.localScale = newScale;
+        // If last value > new value, value has decreased and we display red modification
+        if ((lastTravailScaleY * 100 / goTravailScaleY) > jaugeTravail.Value)
+        {
+            this.goJaugeTravailTemp.color = decreaseColor;
+            this.goJaugeTravail.transform.localScale = newScale;
+            lastTravailScaleY = newScale.y;
+        }
+        // Else if value < new value, value has increased and we display green modification
+        else if ((lastTravailScaleY * 100 / goTravailScaleY) < jaugeTravail.Value)
+        {
+            this.goJaugeTravailTemp.color = increaseColor;
+            this.goJaugeTravailTemp.transform.localScale = newScale;
+            lastTravailScaleY = newScale.y;
+        }
 
+        // ##### SOCIAL ######
+
+        // Both gauges are set to the correct scale
+        newScale = this.goJaugeSocial.transform.localScale;
+        newScale.y = lastSocialScaleY;
+        this.goJaugeSocial.transform.localScale = newScale;
+        this.goJaugeSocialTemp.transform.localScale = newScale;
+        // New scale is calculated
         newScale = this.goJaugeSocial.transform.localScale;
         newScale.y = goSocialScaleY * (jaugeSocial.Value / 100.0f);
-        this.goJaugeSocial.transform.localScale = newScale;
+        // If last value > new value, value has decreased and we display red modification
+        if ((lastSocialScaleY * 100 / goSocialScaleY) > jaugeSocial.Value)
+        {
+            this.goJaugeSocialTemp.color = decreaseColor;
+            this.goJaugeSocial.transform.localScale = newScale;
+            lastSocialScaleY = newScale.y;
+        }
+        // Else if value < new value, value has increased and we display green modification
+        else if ((lastSocialScaleY * 100 / goSocialScaleY) < jaugeSocial.Value)
+        {
+            this.goJaugeSocialTemp.color = increaseColor;
+            this.goJaugeSocialTemp.transform.localScale = newScale;
+            lastSocialScaleY = newScale.y;
+        }
     }
 }
