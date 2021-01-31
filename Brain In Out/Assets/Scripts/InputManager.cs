8082ae071941abdafd20ddfba4e249fcd2822b8f
LouisNMB <baron.louis.pro@gmail.com>
2021-01-31 16:12:06 +0100

    + Les enveloppes glow quand on survole avec la souris

--- Brain In Out/Assets/Scripts/InputManager.cs
+++ Brain In Out/Assets/Scripts/InputManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class InputManager : MonoBehaviour
 {
@@ -19,6 +20,13 @@ public class InputManager : MonoBehaviour
     [SerializeField]
     private AudioSource audioSourceMusic;
 
+    [SerializeField]
+    private List<GameObject> tasEnveloppes;
+    [SerializeField]
+    private List<GameObject> tasEnveloppesGlow;
+
+    private bool glowing = false;
+
     private void Start()
     {
         isOnPause = false;
@@ -28,21 +36,27 @@ public class InputManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && !isOnPause)
-        {
-            //On récupère la position de la souris à l'écran
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
 
-            // On raycast à la position de la souris pour un collicer 2d
-            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
-            if (hit.collider != null )
+
+
+        //On récupère la position de la souris à l'écran
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+        // On raycast à la position de la souris pour un collicer 2d
+        RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+
+        if (hit.collider != null)
+        {
+            DisplayGlow(hit.collider.gameObject);
+            if (Input.GetMouseButtonDown(0) && !isOnPause)
             {
                 ProcessClick(hit.collider.gameObject);
             }
         }
+        else if (glowing)
+            StopGlow();
 
-        if(Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (!isOnPause)
             {
@@ -80,5 +94,30 @@ public class InputManager : MonoBehaviour
         panelPause.SetActive(false);
         audioSourceMusic.Play();
         isGameRunning.Value = prePauseGameRunning;
+    }
+
+    private void DisplayGlow(GameObject go)
+    {
+        if (go.CompareTag("Enveloppe") && !isProcessingLetter.Value)
+        {
+            for (int i = 0; i<tasEnveloppes.Count; i++)
+            {
+                if (tasEnveloppes[i].activeSelf)
+                {
+                    tasEnveloppesGlow[i].SetActive(true);
+                    glowing = true;
+                }
+            }
+        }
+    }
+
+    private void StopGlow()
+    {
+        foreach (GameObject go in tasEnveloppesGlow)
+        {
+            if (go.activeSelf)
+                go.SetActive(false);
+        }
+        glowing = false;
     }
 }
