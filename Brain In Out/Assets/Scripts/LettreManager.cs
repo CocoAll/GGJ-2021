4882b1dfe6c2129b2bbcd35a9f8ad968d5b0dc75
LouisNMB <baron.louis.pro@gmail.com>
2021-01-31 11:28:36 +0100

    + Added random element generation in letters' title and content

--- Brain In Out/Assets/Scripts/LettreManager.cs
+++ Brain In Out/Assets/Scripts/LettreManager.cs
@@ -50,6 +50,17 @@ public class LettreManager : MonoBehaviour
     [SerializeField]
     private IntValue jaugeSocial;
 
+    private List<string> namesList = new List<string> { "Alex", "Sam", "Camille", "Charlie", "Sasha", "Noor", "Amal", "Chams", "Billy", "Cameron", "Elliott", "Gabriel", "Jesse", "Reese", "Azato'th" };
+    private List<string> friendsNamesList = new List<string> { "Amine", "Louis", "Theo", "Corentin", "Anna", "Anjuna", "Moïra", "Louise", "Paul", "Redoine", "Arnaud", "Jean-Michel Jam", "Nassim", "Thery", "Victor", "Ella", "Elisabeth", "Veronica", "Felix", "Kylian", "Anissa", "Loana", "Caroline", "Toby", "Marie", "Jeanne" };
+    private List<string> irritatingBehaviourList = new List<string> { "broke a plate", "cooked the same meal", "forgot to turn off the lights", "chatted with that girl", "chatted with that boy", "used comic sans", "didn't answer me", "stepped on my foot", "made a bad joke" };
+    private List<string> giftList = new List<string> { "a video game", "a pikachu plush", "2 tickets for the Stunfest festival", "a crowbar", "a flag of French Brittany", "a ring to rule them all", "a cupcake", "a kazoo", "a soda can", "a shoe from one of the neighbors' children" };
+    private List<string> storeMerchList = new List<string> { "fish", "shoe", "DVD", "poster", "video game", "hair dryer", "sauce", "phone", "needle", "blanket", "glass", "balloon" };
+    private List<string> videoThemeList = new List<string> { "cat", "dog", "animal", "tiktok", "compilation", "pewdiepie", "unboxing", "minecraft", "political", "fail" };
+    private List<string> outdoorActivityList = new List<string> { "airport", "mall", "movies", "funfair", "gym", "beach" };
+    private List<string> secretList = new List<string> { " do ASMR videos", " like the taste of dog food", " pass out when I eat tofu", " don't know the Lord of the Rings", "'ve never played World of Warcraft", " hate Halloween", " like to drink other peoples' tears", " hate jazz music", " made up the whole story about meeting Toby Fox" };
+    private string friendNameTemp;
+    bool sameFriendName = false;
+
     private void Start()
     {
         enveloppeManager = FindObjectOfType<EnveloppeManager>();
@@ -142,14 +153,14 @@ public class LettreManager : MonoBehaviour
             refouleButton.interactable = true;
         }
 
-        enveloppeTexte.text = currentEnveloppe.value.titre;
+        enveloppeTexte.text = FillRandomTextElements(true);
 
         enveloppeObject.SetActive(true);
     }
 
     public void SetUpLettre()
     {
-        lettreTexte.text = currentEnveloppe.value.contenu;
+        lettreTexte.text = FillRandomTextElements();
         lettreObject.SetActive(true);
     }
 
@@ -169,5 +180,77 @@ public class LettreManager : MonoBehaviour
     {
         lettreObject.SetActive(false);
         enveloppeObject.SetActive(false);
+    }
+
+    private string FillRandomTextElements(bool titre = false)
+    {
+        string letterText;
+        if (titre)
+            letterText = currentEnveloppe.value.titre;
+        else
+            letterText = currentEnveloppe.value.contenu;
+        // crushName
+        if (letterText.Contains("{crushName1}"))
+        {
+            string randomName = namesList[Random.Range(0, namesList.Count)];
+            namesList.Remove(randomName);
+            letterText = letterText.Replace("{crushName1}", randomName);
+            randomName = namesList[Random.Range(0, namesList.Count)];
+            namesList.Remove(randomName);
+            if (namesList.Count == 0)
+                namesList = new List<string> { "Alex", "Sam", "Camille", "Charlie", "Sasha", "Noor", "Amal", "Chams", "Billy", "Cameron", "Elliott", "Gabriel", "Jesse", "Reese", "Azato'th" };
+            letterText = letterText.Replace("{crushName2}", randomName);
+
+        }
+        if (letterText.Contains("{friendName}"))
+        {
+            if (titre)
+            {
+                sameFriendName = true;
+                friendNameTemp = friendsNamesList[Random.Range(0, friendsNamesList.Count)];
+                letterText = letterText.Replace("{friendName}", friendNameTemp);
+            }
+            else if (sameFriendName)
+            {
+                sameFriendName = false;
+                letterText = letterText.Replace("{friendName}", friendNameTemp);
+            }
+            else
+            {
+                string randomName = friendsNamesList[Random.Range(0, friendsNamesList.Count)];
+                letterText = letterText.Replace("{friendName}", randomName);
+            }
+        }
+        if (letterText.Contains("{videoTheme}"))
+        {
+            string randomVideoTheme = videoThemeList[Random.Range(0, videoThemeList.Count)];
+            letterText = letterText.Replace("{videoTheme}", randomVideoTheme);
+        }
+        if (letterText.Contains("{activity}"))
+        {
+            string randomActivity = outdoorActivityList[Random.Range(0, outdoorActivityList.Count)];
+            letterText = letterText.Replace("{activity}", randomActivity);
+        }
+        if (letterText.Contains("{secret}"))
+        {
+            string randomSecret = secretList[Random.Range(0, secretList.Count)];
+            letterText = letterText.Replace("{secret}", randomSecret);
+        }
+        if (letterText.Contains("{gift}"))
+        {
+            string randomSecret = giftList[Random.Range(0, giftList.Count)];
+            letterText = letterText.Replace("{gift}", randomSecret);
+        }
+        if (letterText.Contains("{irritatingBehaviour}"))
+        {
+            string randomSecret = irritatingBehaviourList[Random.Range(0, irritatingBehaviourList.Count)];
+            letterText = letterText.Replace("{irritatingBehaviour}", randomSecret);
+        }
+        if (letterText.Contains("{storeMerch}"))
+        {
+            string randomSecret = storeMerchList[Random.Range(0, storeMerchList.Count)];
+            letterText = letterText.Replace("{storeMerch}", randomSecret);
+        }
+        return letterText;
     }
 }
